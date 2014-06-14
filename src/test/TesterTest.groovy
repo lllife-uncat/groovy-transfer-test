4294c61da71a2ee2e48005ab2673983df31eb410
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-06-14 15:28:04 +0700

    test upload file

--- src/test/TesterTest.groovy
+++ src/test/TesterTest.groovy
@@ -4,7 +4,11 @@ import client.Tester
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import transfer.service.QTransferData
 import transfer.service.QTransferWS
+
+import javax.activation.DataHandler
+import javax.activation.FileDataSource
 
 /**
  * Created by recovery on 6/13/14.
@@ -22,13 +26,34 @@ class TesterTest {
 
     @Test
     def void testBusinessFunction() {
-        def bus = client.getBusinessFunctions()
-        Assert.assertTrue(bus != null);
-        Assert.assertTrue(bus.size() > 0);
+        def i = 0;
+        (1..100).each {
+            def bus = client.getBusinessFunctions()
+            Assert.assertTrue(bus != null);
+            Assert.assertTrue(bus.size() > 0);
+            i++;
+        }
+
+        Assert.assertTrue(i == 100);
     }
 
     @Test
-    def void testEqual() {
-        Assert.assertTrue(0 ==0)
+    def void testUploadFile() {
+        def file = "/home/recovery/projects/transfer.test.groovy/sample/worldcup2014.gif";
+        def fileInfo = new File(file);
+        def data = new QTransferData(
+                clientFileExtension: ".gif",
+                clientFileName: fileInfo.name,
+                clientFilePath: fileInfo.absolutePath,
+                clientMD5: "TestMD5",
+                clientFileSize: fileInfo.getTotalSpace(),
+                clientIP: "TestIPAddress",
+                clientBranch: "TestBranch",
+                clientDepartment: "TestDepartment"
+        )
+        def source = new FileDataSource(file) ;
+        def rs = client.upload(data, new DataHandler(source));
+        def ok = rs.isSuccess()
+        Assert.assertEquals(ok, true);
     }
 }
